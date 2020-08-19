d8be095717fa47e7ef2ba3884151c629ccc54eda
filippodev318 <fili.perna@gmail.com>
2020-08-19 17:27:33 +0200

    AA

--- src/app/auth/login-reg/login-reg.component.ts
+++ src/app/auth/login-reg/login-reg.component.ts
@@ -11,7 +11,7 @@ import {AuthService} from '../../_service/auth-service';
   styleUrls: ['./login-reg.component.css']
 })
 export class LoginRegComponent implements OnInit {
-  backend="http://ciromanfredi96.pythonanywhere.com"
+  backend="https://ciromanfredi96.pythonanywhere.com"
 
   logForm = new FormGroup({
     email:new FormControl("", [
