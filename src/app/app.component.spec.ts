ff66c51f2e7c03d4c768eabe338425bae8291e97
filippodev318 <fili.perna@gmail.com>
2020-08-19 16:47:49 +0200

    aa

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('angular-heroku');
   });
 
   it('should render title', () => {
